589c03246706a201ae793cd3af4e0eecf40c39d0
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2014-11-17 19:42:39 -0500

    test

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname, 'public', 'home.html'));
+	res.sendFile(path.join(__dirname, '../public', 'home.html'));
 });
 
 module.exports = router;
